d0fd6fdca25b63e476b1fca6780c4638d1a7f11c
PuderAlphaBaer <111074541+PuderAlphaBaer@users.noreply.github.com>
2023-02-23 12:50:59 +0100

    better
    noice

--- script.js
+++ script.js
@@ -5,13 +5,41 @@ window.addEventListener("load", function(){
   canvas.height = 720;
 
   class InputHandler {
-    constructor(game){
+    constructor(game) {
       this.game = game;
-      window.addEventListener("keydown", function(e){
-        console.log(e.key)
-      })
+      this.keys = new Set();
+  
+      window.addEventListener("keydown", (event) => {
+        this.keys.add(event.key);
+        this.handleInput();
+      });
+  
+      window.addEventListener("keyup", (event) => {
+        this.keys.delete(event.key);
+        this.handleInput();
+      });
+    }
+  
+    handleInput() {
+      const player = this.game.player;
+      player.speed_x = 0;
+      player.speed_y = 0;
+  
+      if (this.keys.has("ArrowUp")) {
+        player.speed_y = -5;
+      }
+      if (this.keys.has("ArrowDown")) {
+        player.speed_y = 5;
+      }
+      if (this.keys.has("ArrowLeft")) {
+        player.speed_x = -5;
+      }
+      if (this.keys.has("ArrowRight")) {
+        player.speed_x = 5;
+      }
     }
   }
+  
 
   ctx.fillStyle = "white"
   ctx.lineWidth = 3;
@@ -23,6 +51,11 @@ window.addEventListener("load", function(){
       this.collision_x = this.game.width * 0.5;
       this.collision_y = this.game.height * 0.5;
       this.collision_radius = 30;
+      this.speed_x = 0;
+      this.speed_y = 0;
+      this.img = document.getElementById("hund1");
+      this.img_width = 120;
+      this.img_height = 90;
     }
     draw(context){
       context.beginPath();
@@ -31,11 +64,29 @@ window.addEventListener("load", function(){
       context.fill();
       context.restore();
       context.stroke();
+      context.drawImage(this.img, this.collision_x - this.img_width / 2, this.collision_y - this.img_height / 2, this.img_width, this.img_height);
+    }
+    update(){
+      this.collision_x += this.speed_x;
+      this.collision_y += this.speed_y;
+
+      if (this.collision_x - this.collision_radius < 0) {
+        this.collision_x = this.collision_radius;
+      }
+      if (this.collision_x + this.collision_radius > this.game.width) {
+        this.collision_x = this.game.width - this.collision_radius;
+      }
+      if (this.collision_y - this.collision_radius < 0) {
+        this.collision_y = this.collision_radius;
+      }
+      if (this.collision_y + this.collision_radius > this.game.height) {
+        this.collision_y = this.game.height - this.collision_radius;
+      }
     }
   }
 
   class Game {
-    constructor(canvas){
+    constructor(canvas) {
       this.canvas = canvas;
       this.width = this.canvas.width;
       this.height = this.canvas.height;
@@ -43,15 +94,23 @@ window.addEventListener("load", function(){
       this.lastKey = undefined;
       this.input = new InputHandler(this);
     }
-    render(context){
+  
+    render(context) {
       this.player.draw(context);
     }
-  }
-
+  
+    update() {
+      this.player.update();
+    }
+  }  
 
   const game = new Game(canvas);
-  game.render(ctx);
-  function animate(){
 
+  function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update();
+    game.render(ctx);
+    requestAnimationFrame(animate);
   }
+  animate();
 });
